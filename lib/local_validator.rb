95af32ce7a7ecc0d112637949b4e698658d8c792
Takuya Mukohira <takuya.mk96@gmail.com>
2017-08-18 18:44:51 +0900

    Implemented LocalValidator#valid?

--- lib/local_validator.rb
+++ lib/local_validator.rb
@@ -1,3 +1,18 @@
-class LocalValidator
+require './lib/dotatom_validator'
+require './lib/quoted_validator'
 
+class LocalValidator
+  def valid?(str)
+    valid_as_dotatom?(str) || valid_as_quoted?(str)
+  end
+
+  def valid_as_dotatom?(str)
+    validator = DotatomValidator.new
+    validator.valid?(str)
+  end
+
+  def valid_as_quoted?(str)
+    validator = QuotedValidator.new
+    validator.valid?(str)
+  end
 end
--- spec/lib/local_validator_spec.rb
+++ spec/lib/local_validator_spec.rb
@@ -7,5 +7,35 @@ describe LocalValidator do
       is_expected.to be_kind_of LocalValidator
     end
   end
+
+  describe '#valid?' do
+    subject { validator.valid?('dummy') }
+    let(:validator) { LocalValidator.new }
+
+    context 'ドットアトム部が正しいとき' do
+      before do
+        allow(validator).to receive(:valid_as_dotatom?).and_return(true)
+      end
+
+      it { is_expected.to be true }
+    end
+
+    context 'クオーテット部が正しいとき' do
+      before do
+        allow(validator).to receive(:valid_as_quoted?).and_return(true)
+      end
+
+      it { is_expected.to be true }
+    end
+
+    context 'どちらとしても正しくないとき' do
+      before do
+        allow(validator).to receive(:valid_as_dotatom?).and_return(false)
+        allow(validator).to receive(:valid_as_quoted?).and_return(false)
+      end
+
+      it { is_expected.to be false }
+    end
+  end
 end
 
