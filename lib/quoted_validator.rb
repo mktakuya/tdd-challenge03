2f2fb318ecaa469c2ea3254720313eac41ea92c8
Takuya Mukohira <takuya.mk96@gmail.com>
2017-08-18 18:33:43 +0900

    Implemented QuotedValidator

--- lib/quoted_validator.rb
+++ lib/quoted_validator.rb
@@ -1,3 +1,9 @@
 class QuotedValidator
-
+  def valid?(str)
+    !str.nil? &&
+      !str.match(/\A[a-zA-Z0-9!#\$%&'\*\+-\/=\?\^_`\{\|\}~\(\),\.:;<>@\[\]"]*\z/).nil? &&
+      str[0] == '"' &&
+      str[-1] == '"' &&
+      str.size >= 2
+  end
 end
--- spec/lib/quoted_validator_spec.rb
+++ spec/lib/quoted_validator_spec.rb
@@ -10,4 +10,63 @@ describe QuotedValidator do
       is_expected.to be_kind_of QuotedValidator
     end
   end
+
+  describe '#valid?' do
+    subject { validator.valid?(str) }
+    let(:validator) { QuotedValidator.new }
+
+    context '"kuntaro1480" のとき' do
+      let(:str) { '"kuntaro1480"' }
+
+      it { is_expected.to be true }
+    end
+
+    context '".kuntaro" のとき' do
+      let(:str) { '".kuntaro"' }
+
+      it { is_expected.to be true }
+    end
+
+    context '"kuntaro." のとき' do
+      let(:str) { '"kuntaro."' }
+
+      it { is_expected.to be true }
+    end
+
+    context '"kuntaro..ishiyama" のとき ' do
+      let(:str) { '"kuntaro..ishiyama"' }
+
+      it { is_expected.to be true }
+    end
+
+    context '"" のとき' do
+      let(:str) { '""' }
+
+      it { is_expected.to be true }
+    end
+
+    context '"@" のとき' do
+      let(:str) { '"@"' }
+
+      it { is_expected.to be true }
+    end
+
+    context '" のとき' do
+      let(:str) { '"' }
+
+      it { is_expected.to be false }
+    end
+
+    context '"aaaaa のとき' do
+      let(:str) { '"aaaaa' }
+
+      it { is_expected.to be false }
+    end
+
+    context 'aaaaa" のとき' do
+      let(:str) { 'aaaaa"' }
+
+      it { is_expected.to be false }
+    end
+  end
 end
